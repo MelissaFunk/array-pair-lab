a80f7b1a82aa02adb4c239689bc31ab3c1e82ab5
+ <+>
2021-08-12 17:24:16 -0700

    added js code

--- index.js
+++ index.js
@@ -20,11 +20,51 @@ const coursePhases = [
 
 // Write your functions here!
 
+function sortCohortDesc(){
+    ourCohort.sort()    
+    return ourCohort.reverse()
+}
+
+function shortNames(){
+    return ourCohort.filter(ourCohort => ourCohort.length < 15);
+}
+
+function cohortSize(){
+    return ourCohort.length;
+}
+
+function locateStudent(stringArg){
+    return ourCohort.find((student) => student === stringArg);
+}
+
+function phaseNames(){
+    return coursePhases.map(obj => obj['name']);
+}
+
+function cohortPhases(){
+    return coursePhases.filter(obj => obj.language === "JavaScript");
+}
 
 
+function allTheGabes(){
+    return ourCohort.filter(obj => obj.slice(0, 7) === "Gabriel");
+}
 
+function studentAndPhase(){
+    return ourCohort.map(obj => obj + 'is currently in Phase 1!');
+}
 
+function sortPhasesByName(){
+    return coursePhases.sort(function (a, z) {
+        if (a.name < z.name){
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+}   
 
+  
 
 
 // You don't have to change anything below this line
